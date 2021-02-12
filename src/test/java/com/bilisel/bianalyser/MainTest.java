2be203d8d27cabc7fa6d2f66ca4fbc6464f583a1
Bilal Saim <info@bilalsaim.com>
2021-02-12 20:19:28 +0100

    [Hoolah-1] Add userArgumentTest for user to test application easily

--- src/test/java/com/bilisel/bianalyser/MainTest.java
+++ src/test/java/com/bilisel/bianalyser/MainTest.java
@@ -1,7 +1,6 @@
 package com.bilisel.bianalyser;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -15,11 +14,6 @@ public class MainTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
-    @BeforeEach
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
     @AfterEach
     public void restoreStreams() {
         System.setOut(originalOut);
@@ -28,12 +22,13 @@ public class MainTest {
     @Test
     public void testMain() throws IOException {
         // given
+        System.setOut(new PrintStream(outContent));
         String[] args = new String[6];
-        args[0] = "-fromDate";
+        args[0] = "--fromDate";
         args[1] = "20/08/2020 12:00:00";
-        args[2] = "-toDate";
+        args[2] = "--toDate";
         args[3] = "20/08/2020 13:00:00";
-        args[4] = "-merchant";
+        args[4] = "--merchant";
         args[5] = "Kwik-E-Mart";
 
         // when
@@ -42,5 +37,24 @@ public class MainTest {
         // then
         String expectedOutput = String.format("Number of transactions = 1%n" + "Average Transaction Value = 59.99%n");
         assertEquals(outContent.toString(), expectedOutput);
+    }
+
+    @Test
+    public void userArgumentTest() throws IOException {
+        // given
+        String[] args = new String[8];
+        args[0] = "--fromDate";
+        args[1] = "20/08/2020 12:00:00";
+        args[2] = "--toDate";
+        args[3] = "20/08/2020 13:00:00";
+        args[4] = "--merchant";
+        args[5] = "Kwik-E-Mart";
+        args[6] = "--file";
+        args[7] = "";
+
+        // when
+        Main.main(args);
+
+        // then
     }
 }
